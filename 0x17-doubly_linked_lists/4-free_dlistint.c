d3a7e566569bcb36f3f567adaed7a1d459b81d1d
Merciful <lexmerciful@gmail.com>
2022-12-08 14:53:59 -0800

    final

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,16 +1,22 @@
 #include "lists.h"
-/*
- * free_dlistint - frees list
- * @head: head of list to free
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of the list
+ * Return: no return
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
-	tmp = head;
-	while (tmp != NULL)
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
 	{
+		head = head->next;
 		free(tmp);
-		tmp = tmp->next;
 	}
 }
